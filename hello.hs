93135bbb804cfcc8f7aeaa742510924cb1363103
Mislav Novakovic <mislav.novakovic@gmail.com>
2019-10-17 23:58:20 +0300

    implement task 2
    Signed-off-by: Mislav Novakovic <mislav.novakovic@gmail.com>

--- hello.hs
+++ hello.hs
@@ -2,6 +2,6 @@ module Main where
 import System.Environment
 
 main :: IO ()
-main = do args <- getArgs
-          putStrLn ("Hello, " ++ args  !! 0 ++ args !! 1)
-
+main = do
+    args <- getArgs
+    putStrLn(show $ read (args !! 0)  + read (args !! 1))
